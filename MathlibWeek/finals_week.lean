1c72e57f49aaa68df92903783ecf3926382ef0ce
Zane Reeves <zanereeves@Zanes-MacBook-Pro.local>
2023-12-05 02:11:50 -0800

    forgot to save

--- MathlibWeek/finals_week.lean
+++ MathlibWeek/finals_week.lean
@@ -202,3 +202,22 @@ example {G : Type*} [Group G] (H H' : Subgroup G) :
 
 
 -- exercise
+
+def conjugate {G : Type*} [Group G] (x : G) (H : Subgroup G) : Subgroup G where
+  carrier := {a : G | ∃ h, h ∈ H ∧ a = x * h * x⁻¹}
+  one_mem' := by
+    dsimp
+    use 1
+    constructor
+    exact H.one_mem
+    group
+  inv_mem' := by
+    dsimp
+    rintro - ⟨h, h_in, rfl⟩
+    use h⁻¹, H.inv_mem h_in
+    group
+  mul_mem' := by
+    dsimp
+    rintro - - ⟨h, h_in, rfl⟩ ⟨k, k_in, rfl⟩
+    use h*k, H.mul_mem h_in k_in
+    group
